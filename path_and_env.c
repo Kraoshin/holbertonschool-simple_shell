67b61cd4fe55f385fe426b290476bfad13ee81a2
kraoshin <killian.kerleau@outlook.fr>
2024-12-18 11:11:39 +0100

    create a function that supposingly handle path(it dont yet)

--- path_and_env.c
+++ path_and_env.c
@@ -40,4 +40,44 @@ char *_getenv(const char *env_var)
 
 char *is_a_command(char *args)
 {
+	char *env = _getenv("PATH"), *token, *tmp, *path;
+	struct stat st;
 
+	if (!env)
+		return (NULL);
+
+	token = strtok(env, ":");
+
+	while (token)
+	{
+		tmp = malloc(strlen(token) + strlen(args) + 2);
+
+		if (!tmp)
+		{
+			perror("Allocation failed");
+			return (NULL);
+		}
+
+		strcpy(tmp, token);
+		strcat(tmp, "/");
+		strcat(tmp, args);
+
+		if (stat(tmp, &st) == 0)
+		{
+			if (access(tmp, X_OK) == 0)
+			{
+				path = strdup(tmp);
+
+				free(tmp);
+
+				return (path);
+			}
+		}
+
+		free(tmp);
+
+		token = strtok(NULL, ":");
+	}
+
+	return (NULL);
+}
